1cf8957e346bb26849dffd21747b6fe2e04efbe7
satish chouhan <satishchouhanciss@gmail.com>
2022-09-17 17:21:07 +0530

    first

--- app/src/main/java/com/tulsi/batterylevelalarm/MainActivity.java
+++ app/src/main/java/com/tulsi/batterylevelalarm/MainActivity.java
@@ -2,13 +2,22 @@ package com.tulsi.batterylevelalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tulsi.batterylevelalarm.waveloadingview.WaveLoadingView;
 
 public class MainActivity extends AppCompatActivity {
     WaveLoadingView mWaveLoadingView;
+    private Ringtone ringtone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         mWaveLoadingView = (WaveLoadingView) findViewById(R.id.waveLoadingView);
         mWaveLoadingView.setTopTitle("");
         mWaveLoadingView.setCenterTitleColor(Color.WHITE);
-        mWaveLoadingView.setCenterTitle(10+"%");
+
         mWaveLoadingView.setBottomTitleSize(18);
         mWaveLoadingView.setProgressValue(0);
         mWaveLoadingView.setBorderWidth(5);
@@ -32,6 +41,29 @@ public class MainActivity extends AppCompatActivity {
         mWaveLoadingView.resumeAnimation();
         mWaveLoadingView.cancelAnimation();
         mWaveLoadingView.startAnimation();
-        mWaveLoadingView.setProgressValue(50);
+
+
+        ringtone= RingtoneManager.getRingtone(getApplicationContext(),RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
+
+        BroadcastReceiver broadcastReceiverBattery= new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                Integer integerBatteryLevel=intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
+                //textView.setText(.toString());
+                mWaveLoadingView.setCenterTitle(integerBatteryLevel+"%");
+                mWaveLoadingView.setProgressValue(integerBatteryLevel);
+                if(integerBatteryLevel>99||integerBatteryLevel<10)
+                {
+                    ringtone.play();
+                }
+            }
+        };
+        registerReceiver(broadcastReceiverBattery,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+    }
+
+    public void stopAlarm(View view) {
+        ringtone.stop();
     }
 }
